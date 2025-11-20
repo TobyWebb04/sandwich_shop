53f274c0b79b5ac087d81d4d9486671f5ea70980
tobywebb04 <master.toby.webb@gmail.com>
2025-11-20 18:19:18 +0000

    Trying to fix

---
+++ .dart_tool/chrome-device/Default/trusted_vault.pb
@@ -0,0 +1,4 @@
+
+/
++
+)fake-0e3ab4ca-df31-4913-9e1f-9d3b65aedb9b f507ddd9bb79a4c6d777b455d49a8be0,cHcPpuhzE299CXDyt9BUI46QSS9MLAOQx8uE2Kb3zko=
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "N:\\Programming\\sandwich_shop" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=N:\\Programming\\sandwich_shop"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=N:\\Programming\\sandwich_shop\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=N:\\Programming\\sandwich_shop"
+  "FLUTTER_TARGET=N:\\Programming\\sandwich_shop\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049MDVkYjk2ODkwOA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YThiZmRmYzM5NA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjA="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=N:\\Programming\\sandwich_shop\\.dart_tool\\package_config.json"
+)
